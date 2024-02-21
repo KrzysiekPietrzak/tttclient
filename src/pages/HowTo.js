dbeb00e720af9b3f558f366eaa19819cc1cab493
Krzysztof <krzysztof.pietrzak17@gmail.com>
2024-02-21 12:38:41 +0100

    changes paragraph

--- src/pages/HowTo.js
+++ src/pages/HowTo.js
@@ -7,7 +7,7 @@ function App() {
       <Header />
     <div id="howto">
     <h3>Jak grać</h3>
-    <p>fgnasgndjhgnshcfjkh gdjcngfdjhsjgfkdysg  grnhjrskjgrsdgs  gjfgrdjgnvjgrdjsu</p>
+    <p>Instrukcja gry w poniższym materiale video</p>
     <iframe width="560" height="315" src="https://www.youtube.com/embed/alUW0uvzxOg?si=ix7dDvv2CE_LcxeA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 </div >
 <br />
